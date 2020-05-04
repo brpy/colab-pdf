1eef6aaf0aec114af0b31348a6fc46437f9e3c47
brpy <12525622+brpy@users.noreply.github.com>
2020-05-04 13:46:29 +0530

    Update colab_pdf.py

--- colab_pdf.py
+++ colab_pdf.py
@@ -1,6 +1,6 @@
 def colab_pdf(file_name, saveto_Drive = False ,notebookpath = '/content/drive/My Drive/Colab Notebooks/'):
   import os
-  
+  import time
   if(not isinstance(file_name,str)):
     raise TypeError(f"expected a string as file_name, but got {type(file_name)} instead.")
   
@@ -17,6 +17,8 @@ def colab_pdf(file_name, saveto_Drive = False ,notebookpath = '/content/drive/My
   get_ipython().system("apt update && apt install texlive-xetex texlive-fonts-recommended texlive-generic-recommended")
   get_ipython().system("jupyter nbconvert --output-dir='$gdrive_home' '$notebookpath''$file_name' --to pdf")
 
+  time.sleep(3) #Some pdf files take time to appear in the drive
+  
   from google.colab import files
   file_name = file_name.split('.')[0] + '.pdf'
   files.download(gdrive_home+file_name)
