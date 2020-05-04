bf891b8d788408dd7b509abae828fe8fc30566d1
brpy <12525622+brpy@users.noreply.github.com>
2020-05-04 14:45:30 +0530

    Update colab_pdf.py

--- colab_pdf.py
+++ colab_pdf.py
@@ -15,11 +15,17 @@ def colab_pdf(file_name, notebookpath = '/content/drive/My Drive/Colab Notebooks
     raise ValueError(f"file '{file_name}' not found in path '{notebookpath}'.")
 
   get_ipython().system("apt update && apt install texlive-xetex texlive-fonts-recommended texlive-generic-recommended")
-  get_ipython().system("jupyter nbconvert --output-dir='$gdrive_home' '$notebookpath''$file_name' --to pdf")
+  
+  try:
+    get_ipython().system("jupyter nbconvert --output-dir='$gdrive_home' '$notebookpath''$file_name' --to pdf")
+  except:
+    return("nbconvert error")
   
   try:
     from google.colab import files
     file_name = file_name.split('.')[0] + '.pdf'
     files.download(gdrive_home+file_name)
+  except:
+    return("File Download Unsuccessful. Saved in Google Drive")
   
-  return("File Downloaded to Drive")
+  return("File ready to be Downloaded and Saved to Drive")
