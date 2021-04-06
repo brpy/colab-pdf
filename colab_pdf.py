13a255f944345dd0ea2468573f8e34ccf4fb3b74
pranavdg1997 <pgujarat@iu.edu>
2021-04-06 14:49:56 -0400

    Update colab_pdf.py

--- colab_pdf.py
+++ colab_pdf.py
@@ -8,8 +8,8 @@ def colab_pdf(file_name, notebookpath="/content/drive/My Drive/Colab Notebooks/"
         )
 
     # Using the defaults used by google.colab
-    drive_mount_point = "/content/drive/"
-    gdrive_home = os.path.join(drive_mount_point, "My Drive/")
+    drive_mount_point = "/drive/"
+    gdrive_home = os.path.join(drive_mount_point, "MyDrive/")
 
     # If the drive is not already mounted, attempt to mount it.
     if not os.path.isdir(gdrive_home):
